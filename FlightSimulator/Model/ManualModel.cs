b163269c27a7c7c9f2e05af705e949207a660b1f
Ran Raboh <ran.raboh@live.biu.ac.il>
2019-04-14 17:01:50 +0300

    set to server

---
+++ FlightSimulator/Model/ManualModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    class ManualModel
+    {
+        private CommandsServer server;
+        public ManualModel()
+        {
+            server = CommandsServer.Instance;
+        }
+        public void SetProperty(string property, double value)
+        {
+            string command = "set " + property + " " + value + "\r\n";
+            server.write(command);
+        }
+    }
+}
